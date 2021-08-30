2fbde9ed7733edb28cf969db5ff39af86826c432
hiroyuki takai <41976285+htakai@users.noreply.github.com>
2021-08-30 23:33:08 +0900

    Create template-functions.php

---
+++ undersample/inc/template-functions.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Functions which enhance the theme by hooking into WordPress
+ *
+ * @package undersample
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function undersample_body_classes( $classes ) {
+	// Adds a class of hfeed to non-singular pages.
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+
+	// Adds a class of no-sidebar when there is no sidebar present.
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		$classes[] = 'no-sidebar';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'undersample_body_classes' );
+
+/**
+ * Add a pingback url auto-discovery header for single posts, pages, or attachments.
+ */
+function undersample_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+	}
+}
+add_action( 'wp_head', 'undersample_pingback_header' );
+
+/**
+ * Change word for more_tag.
+ *
+ * @param string $more describe more tag.
+ */
+function undersample_excerpt_more( $more ) {
+	return '...etc';
+}
+add_filter( 'excerpt_more', 'undersample_excerpt_more' );
